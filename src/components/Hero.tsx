28628101e574083aa90af8b70eedd3bca45653ec
Ashen06C <ashencharanga99@gmail.com>
2024-09-09 22:04:53 +0530

    complete to about us

---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+import Info from "./Info";
+
+const Hero = () => {
+  return (
+    <div>
+      <div
+        className="relative bg-cover bg-center h-screen text-white flex flex-col items-center justify-center pt-12"
+        style={{ backgroundImage: `url('/images/hero-1.jfif')` }}
+      >
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+
+        {/* Content */}
+        <div className="relative z-10 text-center">
+          <h1 className="text-7xl font-serif">
+            Let Your Hair <span className="text-orange-300">Mirror</span> Your
+          </h1>
+          <h1 className="text-7xl font-serif pb-4 ">Heart's Desire</h1>
+          <div></div>
+          <button className="mt-6 border py-2 px-6 rounded-md 0 text-white  hover:scale-105 hover:shadow-lg transition-transform duration-300">
+            Book Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
